321b4098feda04a3b512e1538d31ab19841ab556
danyraR06 <daniirmz06@gmail.com>
2015-02-23 23:21:58 -0600

    main que ya corre

--- src/JuegoBreaking.java
+++ src/JuegoBreaking.java
@@ -1,3 +1,6 @@
+
+import java.util.LinkedList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,19 @@
  * @author danyrmz
  */
 public class JuegoBreaking {
+    private int iPosX;
+    private int iPosY;
+    private int iContAnf;
+    private int iDireccion;
+    private boolean bolPause;
+    private boolean bolEnd;
+    private static final int iAltoJ = 1000;
+    private static final int iAnchoJ = 800;
+    private Main maiPills;
+    private Main maiBarrilla;
+    private Main maiFire;
+    
+    private LinkedList <Main> lklAnfetaminas;
+    
     
 }
